c3160f4c568cf56d650e372925bcf0f81c044a4d
xindongol <xindongemail@gmail.com>
2020-04-14 20:50:39 +0000

    exp

---
+++ exp/main.py
@@ -0,0 +1,283 @@
+import time
+import copy
+import sys
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import numpy as np
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+# Using CIFAR-10 again as in Assignment 1
+# Load training data
+transform_train = transforms.Compose([                                   
+    transforms.RandomCrop(32, padding=4),                                       
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, 
+                                        download=True,
+                                        transform=transform_train)
+
+# Load testing data
+transform_test = transforms.Compose([                                           
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True,
+                                       transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False,
+                                         num_workers=2)
+
+
+# Using same ConvNet as in Assignment 1
+def conv_block(in_channels, out_channels, kernel_size=3, stride=1,
+               padding=1):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding,
+                  bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+        )
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.model = nn.Sequential(
+            conv_block(3, 32),
+            conv_block(32, 32),
+            conv_block(32, 64, stride=2),
+            conv_block(64, 64),
+            conv_block(64, 64),
+            conv_block(64, 128, stride=2),
+            conv_block(128, 128),
+            conv_block(128, 256),
+            conv_block(256, 256),
+            nn.AdaptiveAvgPool2d(1)
+            )
+
+        self.classifier = nn.Linear(256, 10)
+
+    def forward(self, x):
+        h = self.model(x)
+        B, C, _, _ = h.shape
+        h = h.view(B, C)
+        return self.classifier(h)
+    
+    
+class DatasetSplit(torch.utils.data.Dataset):
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = [int(i) for i in idxs]
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[self.idxs[item]]
+        return image, torch.tensor(label)
+  
+def create_device(net, device_id, trainset, data_idxs, lr=0.1,
+                  milestones=None, batch_size=128):
+    if milestones == None:
+        milestones = [25, 50, 75]
+
+    device_net = copy.deepcopy(net)
+    optimizer = torch.optim.SGD(device_net.parameters(), lr=lr, momentum=0.9,
+                                weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                                     milestones=milestones,
+                                                     gamma=0.1)
+    device_trainset = DatasetSplit(trainset, data_idxs)
+    device_trainloader = torch.utils.data.DataLoader(device_trainset,
+                                                     batch_size=batch_size,
+                                                     shuffle=True,
+                                                     num_workers=2)
+    return {
+        'net': device_net,
+        'id': device_id,
+        'dataloader': device_trainloader, 
+        'optimizer': optimizer,
+        'scheduler': scheduler,
+        'train_loss_tracker': [],
+        'train_acc_tracker': [],
+        'test_loss_tracker': [],
+        'test_acc_tracker': [],
+        }
+  
+def train(epoch, device):
+    net.train()
+    train_loss, correct, total = 0, 0, 0
+    for batch_idx, (inputs, targets) in enumerate(device['dataloader']):
+        inputs, targets = inputs.cuda(), targets.cuda()
+        device['optimizer'].zero_grad()
+        outputs = device['net'](inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        device['optimizer'].step()
+        train_loss += loss.item()
+        device['train_loss_tracker'].append(loss.item())
+        loss = train_loss / (batch_idx + 1)
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+        acc = 100. * correct / total
+        dev_id = device['id']
+        sys.stdout.write(f'\r(Device {dev_id}/Epoch {epoch}) ' + 
+                         f'Train Loss: {loss:.3f} | Train Acc: {acc:.3f}')
+        sys.stdout.flush()
+    device['train_acc_tracker'].append(acc)
+    sys.stdout.flush()
+
+def test(epoch, device):
+    net.eval()
+    test_loss, correct, total = 0, 0, 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.cuda(), targets.cuda()
+            outputs = device['net'](inputs)
+            loss = criterion(outputs, targets)
+            test_loss += loss.item()
+            device['test_loss_tracker'].append(loss.item())
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            loss = test_loss / (batch_idx + 1)
+            acc = 100.* correct / total
+    sys.stdout.write(f' | Test Loss: {loss:.3f} | Test Acc: {acc:.3f}\n')
+    sys.stdout.flush()  
+    acc = 100.*correct/total
+    device['test_acc_tracker'].append(acc)
+    
+    
+def iid_sampler(dataset, num_devices, data_pct):
+    '''
+    dataset: PyTorch Dataset (e.g., CIFAR-10 training set)
+    num_devices: integer number of devices to create subsets for
+    data_pct: percentage of training samples to give each device
+              e.g., 0.1 represents 10%
+
+    return: a dictionary of the following format:
+      {
+        0: [3, 65, 2233, ..., 22] // device 0 sample indexes
+        1: [0, 2, 4, ..., 583] // device 1 sample indexes
+        ...
+      }
+
+    iid (independent and identically distributed) means that the indexes
+    should be drawn independently in a uniformly random fashion.
+    '''
+
+    # total number of samples in the dataset
+    total_samples = len(dataset)
+
+    # Part 1.1: Implement!
+    device_samples = round(data_pct * total_samples)
+    return {i: torch.randperm(total_samples)[:device_samples].tolist() 
+            for i in range(num_devices)}
+    
+data_pct = 0.1
+epochs = 1000
+num_devices = 1
+device_pct = 0.1
+net = ConvNet().cuda()
+criterion = nn.CrossEntropyLoss()
+
+# Part 1.1: Implement cifar_idd to generate data_idxs for create_device
+data_idxs = iid_sampler(trainset, num_devices, data_pct)
+
+# Part 1.1: Create the device
+device = create_device(net, 0, trainset, data_idxs[0],
+                       milestones=[250, 500, 750])
+
+# Part 1.1: Train the device model for 100 epochs and plot the result
+# Standard Training Loop
+start_time = time.time()
+for epoch in range(epochs):
+    train(epoch, device)
+    # To speed up running time, only evaluate the test set every 10 epochs
+    if epoch > 0 and epoch % 10 == 0:
+        test(epoch, device)
+    device['scheduler'].step()
+    
+    
+def average_weights(devices):
+    '''
+    Returns the average of the weights.
+    '''
+    # Part 1.2: Implement!
+    w = [device['net'].state_dict() for device in devices]
+    w_avg = copy.deepcopy(w[0])
+    for key in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[key] += w[i][key]
+        w_avg[key] = torch.div(w_avg[key], len(w))
+        
+    return w_avg
+
+def get_devices_for_round(devices, device_pct):
+    '''
+    '''
+    # Part 1.2: Implement!
+    num_devices_in_round = round(device_pct*len(devices))
+    device_idxs = np.random.permutation(len(devices))[:num_devices_in_round]
+    return [devices[i] for i in device_idxs]
+
+
+total_time = time.time() - start_time
+print('Total training time: {} seconds'.format(total_time))
+
+# plotting code
+plt.plot(device['test_acc_tracker'])
+plt.xlabel('Round')
+plt.ylabel('Classification Accuracy')
+
+
+rounds = 100
+local_epochs = 4
+num_devices = 50
+device_pct = 0.1
+data_pct = 0.1
+net = ConvNet().cuda()
+criterion = nn.CrossEntropyLoss()
+
+data_idxs = iid_sampler(trainset, num_devices, data_pct)
+
+# Part 1.3: Implement device creation here
+devices = [create_device(net, i, trainset, data_idxs[i])
+           for i in range(num_devices)]
+
+## IID Federated Learning
+start_time = time.time()
+for round_num in range(rounds):
+    # Part 1.3: Implement getting devices for each round here
+    round_devices = get_devices_for_round(devices, device_pct)
+
+    print('Round: ', round_num)
+    for device in round_devices:
+        for local_epoch in range(local_epochs):
+            train(local_epoch, device)
+
+    # Part 1.3: Implement weight averaging here
+    w_avg = average_weights(round_devices)
+
+    for device in devices:
+        device['net'].load_state_dict(w_avg)
+        device['optimizer'].zero_grad()
+        device['optimizer'].step()
+        device['scheduler'].step()
+
+    # test accuracy after aggregation
+    test(round_num, devices[0])
+
+
+total_time = time.time() - start_time
+print('Total training time: {} seconds'.format(total_time))
+plt.plot('test_acc_tracker')
